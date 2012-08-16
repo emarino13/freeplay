e05defc9806009e6783b203e4867f277ff5c6b4f
Elaine Marino <elaine.c.marino@gmail.com>
2012-08-15 18:36:59 -0600

    Refactor another method to give it a name. This is good to do to improve code quality and ease of reading for later developers

--- example/dummy.rb
+++ example/dummy.rb
@@ -7,14 +7,10 @@ class Dummy < Freeplay::Player
   def move
     x, y = nil, nil
 
-    # First try to move to a space adjacent to my opponent's last move.
     if board.last_opponent_move
       logger.info("searching for an open adjacent space")
 
-      allowed = board.adjacent(*board.last_opponent_move)
-      match = allowed.detect {|(ax, ay)| board[ax, ay] == :empty}
-      x, y = match if match
-     # x, y = find_space_adjacent_to_opponents_last_move(board.last_opponent_move)
+      x, y = find_space_adjacent_to_opponents_last_move(board.last_opponent_move)
     end
 
     if x.nil? or y.nil?
@@ -30,7 +26,11 @@ class Dummy < Freeplay::Player
   def find_space_adjacent_to_opponents_last_move(opponent_last_move)
       allowed = board.adjacent(*opponent_last_move)
       match = allowed.detect {|(ax, ay)| board[ax, ay] == :empty}
-      x, y = match if match
+      if match
+        match
+      else
+        [nil, nil]
+      end
   end
 
   def find_first_available_space
